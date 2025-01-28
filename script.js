7b100a1e5d732373ede8f087287b4e0f6815e332
Benjakronk <benjamin_ensrud@hotmail.com>
2025-01-28 22:29:07 +0100

    Cosmic update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const TYPE_COLORS = {
     grass: '#78C850', ice: '#98D8D8', fighting: '#C03028', poison: '#A040A0',
     ground: '#E0C068', flying: '#A890F0', psychic: '#F85888', bug: '#A8B820',
     rock: '#B8A038', ghost: '#705898', dragon: '#7038F8', dark: '#705848',
-    steel: '#B8B8D0', fairy: '#EE99AC'
+    steel: '#B8B8D0', fairy: '#EE99AC', cosmic: '#2E1F5E'
 };
 const IMAGE_BASE_URL = 'https://raw.githubusercontent.com/Benjakronk/shima-pokedex/main/images/';
 const IMAGE_FORMATS = ['png', 'jpg', 'jpeg', 'jfif'];
